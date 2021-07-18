9d3fd704471f496ccef7b098a2f67556ed00320f
Alex <Alex@DESKTOP-716HUTS>
2021-07-18 22:46:17 +0200

    First Commit

---
+++ src/components/todoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const todoList = () => {
+    return(
+        <div className="todo-container">
+            <ul className="todo-list">
+
+            </ul>
+        </div>
+    );
+}
+
+export default todoList;
